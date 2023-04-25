4789158fe91a00ea8655eb65a7629f180c37b9bd
JuanRamos15 <124464419+JuanRamos15@users.noreply.github.com>
2023-04-25 14:47:03 +0000

    buil

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ import path from 'path';
 
 //recreando Built-in variables
 global["_dirname"]=path.dirname(new URL(import.meta.url).pathname);
-global["_dirname"]=path.basename(new URL(import.meta.url).pathname);
+global["_dirname"]=path.join(_dirname,path.basename(new URL(import.meta.url).pathname));
 // 2. Crear el servidor
- //
 const server = http.createServer( (req, res)=>{
     // Toda la logica del server
     // 1. Respondiendo al cliente
-    res.write("Hello from the server... ");
+    res.write("Revisa la consola server ");
     // 2. Cerrar la conexion
+    console.log(`client .request: ${req.method}${req.url}`);
     res.end();
 } );
 
